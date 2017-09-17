57921c641b6e9203dfbba8fa492b877bc7c9e49a
sennseikun <thomasbo@hotmail.no>
2017-09-17 15:22:29 +0200

    update

--- PS1files/bitmap.h
+++ PS1files/bitmap.h
@@ -4,7 +4,7 @@
 #include "julia_handout.h"
 
 typedef unsigned char uchar;
-void savebmp(char *name,uchar *buffer,int x,int y);
-void fancycolour(uchar *p,int iter);
+//void savebmp(char *name,uchar *buffer,int x,int y);
+//void fancycolour(uchar *p,int iter);
 
 #endif
